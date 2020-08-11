003fa012f53010d31287f12e119f1786bd260366
DanBi-Lee <danbi.s.rain@gmail.com>
2020-08-11 09:40:01 +0900

    Updates

---
+++ precache-manifest.828acaeaa3ba7d3469ea62e6cab03065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac627a03dfde084c75fe582957dba2c3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "223c835adb9a4040990c",
+    "url": "/movie_app/static/css/main.59e30f01.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223c835adb9a4040990c",
+    "url": "/movie_app/static/js/main.73406d0c.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
